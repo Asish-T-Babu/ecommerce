8cbc0ce4575cbde1c4cc99b2128eb9a29592feb9
Asish-T-Babu <asishtbabu@gmail.com>
2024-08-26 00:40:01 +0530

    Updated settings.py file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -131,6 +131,12 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
+# MEDIA_ROOT is where files are stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# MEDIA_URL is the URL to access media files
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
